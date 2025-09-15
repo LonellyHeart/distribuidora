f4f7e3c5d71f30c7054da2ef5f2249a3693877d7
“Matheus <matheus.ricci@hotmail.com>
2025-09-15 11:41:12 -0300

    Commit inicial

---
+++ src/test/java/com/etapa9/distribuidora/DistribuidoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etapa9.distribuidora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
